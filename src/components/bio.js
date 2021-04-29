4c93698ebfb9b8964a7e20de2b8558be7385af01
matt <matthew.f.prado@valkyriepcs.com>
2021-04-29 08:37:09 -0700

    added other social media links

--- src/components/bio.js
+++ src/components/bio.js
@@ -45,10 +45,19 @@ const Bio = () => {
       {author?.name && (
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
+          <div className="bio-links">
           <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
+            Twitter
           </a>
+          {`  `}
+          <a href={`https://instagram.com/${social?.instagram || ``}`}>
+            Instagram
+          </a>
+            {`  `}
+            <a href={`https://github.com/${social?.github || ``}`}>
+              GitHub
+            </a>
+          </div>
         </p>
       )}
     </div>
